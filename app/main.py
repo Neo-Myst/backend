c639eda28af2ea56ad7b348efe4dc336507b645f
Vedant-Mhatre <vedantmh@gmail.com>
2025-05-06 22:07:02 -0400

    temporary refactor(middleware): simplify CORS allow_origins to "*"

--- app/main.py
+++ app/main.py
@@ -31,17 +31,7 @@ app = FastAPI(lifespan=lifespan)
 # Middleware configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://localhost:80",
-        "http://localhost:443",
-        "http://localhost",
-        "http://10.0.0.12",         # Frontend EC2 instance private IP
-        "http://10.0.0.12:3000",    # In case you're using a specific port
-        "http://10.0.0.12:5173",    # If using Vite's default port
-        "http://10.0.0.12:80",      # Standard HTTP port
-        "http://10.0.0.12:443"      # HTTPS port
-    ],
+    allow_origins=["*"],  # Allow all origins
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
